c7ab18261dcffb8c2591d2f8db59c85d8b8af1f5
Maurizio Tolomeo <37340833+moris88@users.noreply.github.com>
2022-11-21 18:14:08 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 console.log('myCode!!!')
+const newArray = chunk(10,3)
+console.log(newArray)
+
+function chunk(totalNumber: number, nChunk: number) {
+  const result: number[][] = []
+  let listNumber: number[] = []
+  let count = 0
+  for (let i = 1; i <= totalNumber; i++) {
+    count = listNumber.length
+    if (count === nChunk) {
+      result.push(listNumber)
+      listNumber = []
+    }
+    listNumber.push(i)
+    if (i === totalNumber) {
+      result.push(listNumber)
+    }
+  }
+  return result
+} 
